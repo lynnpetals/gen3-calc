d03eae8c64c434cf26dcd07ac7314e2d147c8376
Lynn <afalseprayer@gmail.com>
2024-06-29 13:05:32 -0400

    push 30

--- js/shared_controls.js
+++ js/shared_controls.js
@@ -1588,7 +1588,9 @@ function getMoveDetails(moveInfo, species, ability, item, useMax) {
 
 function createField() {
 	var gameType = $("input:radio[name='format']:checked").val();
-	var game = $("input[name='game']:checked + label").html();
+	// FROG
+	var doc = document.getElementById("gameSelection")
+	game = (doc).options[doc.selectedIndex].text
 	var isBeadsOfRuin = $("#beads").prop("checked");
 	var isTabletsOfRuin = $("#tablets").prop("checked");
 	var isSwordOfRuin = $("#sword").prop("checked");
@@ -2946,14 +2948,19 @@ $(".stat-changer").click((e) => {
 
 window.isInDoubles = false;
 function switchIconSingle() {
-	document.getElementById("monDouble").removeAttribute("hidden");
+	// document.getElementById("monDouble").removeAttribute("hidden");
+	
+	$(".speedText").attr("hidden", false)
+	$(".speedIcon").attr("hidden", true)
 	window.isInDoubles = true;
 	return;
 }
 
 function switchIconDouble() {
-	document.getElementById("monDouble").setAttribute("hidden", true);
+	// document.getElementById("monDouble").setAttribute("hidden", true);
 	window.isInDoubles = false;
+	$(".speedText").attr("hidden", true)
+	$(".speedIcon").attr("hidden", false)
 	return;
 }
 
@@ -2964,6 +2971,19 @@ function openCloseItemBox() {
 function openCloseNoteBox() {
 	document.getElementById("note-box-frame").toggleAttribute("hidden");
 }
+
+$(".totalMod").mouseenter((e) => {
+	if($(e.target)[0].className){
+		$(".speedText").attr("hidden", false)
+	}
+	console.log($(e.target)[0].className)
+})
+$(".totalMod").mouseleave((e) => {
+	if($(e.target)[0].className){
+		$(".speedText").attr("hidden", true)
+	}
+	console.log($(e.target)[0].className)
+})
 
 function selectItem(ev) {
 	var newItem = ev.target.getAttribute("data-id");
