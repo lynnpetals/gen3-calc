aa28462aa7c131fcfe0344ffa38fb0eec6c32d6d
AFalsePrayer <89038954+AFalsePrayer@users.noreply.github.com>
2024-06-30 15:23:20 +0000

    Deploy 7d35297

---
+++ js/data/sets/gen5.js
@@ -0,0 +1,1 @@
+var SETDEX_BW = {"Ivysaur":{"ZU Physical Wall":{"ability":"Overgrow","item":"Eviolite","nature":"Bold","evs":{"hp":252,"df":252,"sd":4},"moves":["Knock Off","Sludge Bomb","Synthesis","Giga Drain"]}}};
---
+++ js/data/sets/gen6.js
@@ -0,0 +1,1 @@
+var SETDEX_XY = {"Arbok":{"NU Offensive Coil":{"ability":"Intimidate","item":"Life Orb","nature":"Adamant","evs":{"at":252,"sd":4,"sp":252},"moves":["Coil","Gunk Shot","Earthquake","Sucker Punch"]}}};
